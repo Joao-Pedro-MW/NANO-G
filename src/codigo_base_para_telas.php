b59034fe7f1b43e1a5c2d7121a64c586a1ac079f
João Pedro MW <jpedro01230@gmail.com>
2023-06-08 20:44:38 -0300

    Finalizado codigo base do menu

---
+++ src/codigo_base_para_telas.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Menu</title>
+	<link rel="stylesheet" type="text/css" href="css\menu\header.css">
+	<script type="text/javascript" src="js\menu.js"></script>
+</head>
+<body>
+	<?php require 'menu_lateral.html' ?>
+</body>
+</html>
