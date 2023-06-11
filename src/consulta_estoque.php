cc69c65112c04ce4b0cf2cc56c9333c75f14f254
João Pedro MW <80231608+Joao-Pedro-MW@users.noreply.github.com>
2023-06-11 20:45:11 -0300

    Ajustes do sistema (#9)
    * Update consulta_estoque.php
    
    * Ajustes realizados
    
    * Update estoque.css
    
    * AJUSTE
    
    * Update cores.css
    
    * Ajustado erros
    
    * Realizando testes
    
    * Update consulta_estoque.php
    
    * Update cores.css
    
    * Update estoque.css
    
    * Ajustes de cores dos botões
    
    * Update cores.css
    
    * Update cores.css

--- src/consulta_estoque.php
+++ src/consulta_estoque.php
@@ -3,9 +3,9 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Logs</title>
+	<title>Consulta estoque</title>
 	<link rel="stylesheet" type="text/css" href="css\menu\header.css">
-	<link rel="stylesheet" type="text/css" href="css\logs\logs.css">
+	<link rel="stylesheet" type="text/css" href="css\estoque\estoque.css">
 	<script type="text/javascript" src="js\menu.js"></script>
 </head>
 <body>
