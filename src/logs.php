3fab2130185989f583a0cb21dc3305080b09d14e
João Pedro MW <80231608+Joao-Pedro-MW@users.noreply.github.com>
2023-06-11 17:20:35 -0300

    União da tela de logs do sistema (#4)
    * Arquivos base criados e alguns editados
    
    * Ajustado CSS
    
    Removido imagem do filtro, não consegui estilizar

---
+++ src/logs.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Logs</title>
+	<link rel="stylesheet" type="text/css" href="css\menu\header.css">
+	<link rel="stylesheet" type="text/css" href="css\logs\logs.css">
+	<script type="text/javascript" src="js\menu.js"></script>
+</head>
+<body>
+	<?php require 'menu_lateral.html' ?>
+   	<div class="base">
+	<h1>Registros de Entrada/Saída</h1>
+	<div class="menu_botoes">
+		<button type="button" id="saida" class="botao">Saída</button>
+		<button type="button" id="entrada" class="botao">Entrada</button>
+		<p>De:</p>
+		<input type="date" name="data_inicio" id="data_inicio" class="botao" data-placeholder="Data Inicial">
+		<p>até:</p>
+		<input type="date" name="data_fim" id="data_fim" class="botao" placeholder="Data Final">
+		<button type="button" class="botao" id="botao_filtro">Filtrar Dados
+			<!--<img id="imagem_filtro" src="imagens\imagem_filtro.svg" alt="imagem de filtro">-->
+		</button>
+	</div>
+	<table class="tabela_dados" id="tabela_dados">
+		<tr>
+			<th>Nome</th>
+			<th>Valor</th>
+			<th>Quantidade</th>
+			<th>Vencimento</th>
+			<th>Lote</th>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+		</tr>
+
+	</table>
+	</div>
+</body>
+</html>
