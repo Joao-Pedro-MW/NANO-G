4051c1c4f741e01c3f60f167f18668ae13fdb089
Alynne <alynneak@outlook.com>
2023-06-13 19:38:55 -0300

    inserção das telas base de usuário

---
+++ src/cadastro_usuarios.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Usuário</title>
+	<link rel="stylesheet" type="text/css" href="css\menu\header.css">
+	<script type="text/javascript" src="js\menu.js"></script>
+</head>
+<body>
+	<?php require 'menu_lateral.html' ?>
+   	<div class="base"></div>
+
+
+
+    <!--
+        Campo para foto do usuário
+        Nome
+        CPF
+        E-mail
+        Data Nascimento
+        Tipo Usuário
+
+        Botão
+    --> 
+</body>
+</html>
