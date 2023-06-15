404905bd6f18467e2abe4970889a8c7e6c486a12
João Pedro MW <80231608+Joao-Pedro-MW@users.noreply.github.com>
2023-06-15 20:51:16 -0300

    Ajustes do bug de do estoque (#14)
    * Ajustado responsividade
    
    * Update estoque.css
    
    * Ajustado responsividade
    
    * Ajuste de responsividade do layout
    
    * Margem para os arquivos e margin 0 para móvel
    
    * Revert "Merge branch 'main' into J-Consulta-Estoque"
    
    This reverts commit 412e7dc4d59cc559e761a6838bfa752f6ae72104, reversing
    changes made to 9c24a70f062bb069dde282c9d99e2588aebd3775.

--- src/consulta_estoque.php
+++ src/consulta_estoque.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Estoque</title>
+	<title>Consulta estoque</title>
 	<link rel="stylesheet" type="text/css" href="css\menu\header.css">
 	<link rel="stylesheet" type="text/css" href="css\estoque\estoque.css">
 	<script type="text/javascript" src="js\menu.js"></script>
