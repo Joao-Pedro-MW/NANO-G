3dadf9e04a5882d5ade95b3d618bd50c6b48b4af
João Pedro MW <80231608+Joao-Pedro-MW@users.noreply.github.com>
2023-07-05 19:20:36 -0300

    Criado menu (#20)
    Criado CSS, layout base, estilizado tela para desktop

---
+++ src/menu_produtos.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Menu</title>
+	<link rel="stylesheet" type="text/css" href="css\menu\header.css">
+    <link rel="stylesheet" type="text/css" href="css\menu_produtos\css_produtos.css">
+	<script type="text/javascript" src="js\menu.js"></script>
+</head>
+<body>
+	<?php require 'menu_lateral.html' ?>
+   	<div class="base">
+        <h1>Consulta de produtos</h1>
+        <button type="button" class="botao">Cadastrar novo produto</button>
+        <table class="tabela_dados" id="tabela_dados">
+			<tr>
+				<th>Nome</th>
+				<th>Unidade de Medida</th>
+				<th>Tipo</th>
+				<th>Operações</th>
+			</tr>
+			<tr>
+				<td class="td_produto">Produto 1</td>
+				<td class="td_un_medida">Kg</td>
+				<td class="td_tipo">Materia-Prima</td>
+				<td class="td_opcoes">
+                    <img src="imagens\imagem_lapis_edicao.svg" alt="Lapis" id="icones">
+                    <img src="imagens\imagem_lixeira.svg" alt="Lixeira" id="icones">
+                </td>
+			</tr>
+            <tr>
+				<td class="td_produto">Produto 1</td>
+				<td class="td_un_medida">Kg</td>
+				<td class="td_tipo">Materia-Prima</td>
+				<td class="td_opcoes">
+                    <img src="imagens\imagem_lapis_edicao.svg" alt="Lapis" id="icones">
+                    <img src="imagens\imagem_lixeira.svg" alt="Lixeira" id="icones">
+                </td>
+			</tr>
+            <tr>
+				<td class="td_produto">Produto 1</td>
+				<td class="td_un_medida">Kg</td>
+				<td class="td_tipo">Materia-Prima</td>
+				<td class="td_opcoes">
+                    <img src="imagens\imagem_lapis_edicao.svg" alt="Lapis" id="icones">
+                    <img src="imagens\imagem_lixeira.svg" alt="Lixeira" id="icones">
+                </td>
+			</tr>
+            <tr>
+				<td class="td_produto">Produto 1</td>
+				<td class="td_un_medida">Kg</td>
+				<td class="td_tipo">Materia-Prima</td>
+				<td class="td_opcoes">
+                    <img src="imagens\imagem_lapis_edicao.svg" alt="Lapis" id="icones">
+                    <img src="imagens\imagem_lixeira.svg" alt="Lixeira" id="icones">
+                </td>
+			</tr>
+        </table>
+    </div>
+</body>
+</html>
