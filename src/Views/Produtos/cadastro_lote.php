96fa4a5553823e82f74b451e6764a6e24dbe79d2
João Pedro MW <jpedro01230@gmail.com>
2023-09-26 22:49:04 -0300

    Ajustes

--- src/Views/Produtos/cadastro_lote.php
+++ src/Views/Produtos/cadastro_lote.php
@@ -10,7 +10,7 @@
 </head>
 
 <body>
-	<?php require __DIR__ . '..\..\menu_lateral.php' ?>
+	<?php require __DIR__ . '..\..\..\menu_lateral.php' ?>
 	<div class="linha">
 		<div class="coluna">
 			<div>
