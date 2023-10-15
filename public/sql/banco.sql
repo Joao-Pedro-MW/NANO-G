2f5d158657625c0c24ad318137727f7845449d3d
Alynne <100451601+AlynneAyumi@users.noreply.github.com>
2023-10-15 20:12:04 -0300

    Update banco.sql

--- public/sql/banco.sql
+++ public/sql/banco.sql
@@ -4,7 +4,7 @@ CREATE SEQUENCE s_un_medida_id START WITH 1 INCREMENT BY 1 NOCACHE NOCYCLE;
 CREATE SEQUENCE s_categoria_id START WITH 1 INCREMENT BY 1 NOCACHE NOCYCLE;
 CREATE SEQUENCE s_lote_id START WITH 100 INCREMENT BY 1 NOCACHE NOCYCLE;
 
-CREATE TABLE IF NOT EXISTS n_usuarios (
+CREATE TABLE IF NOT EXISTS n_usuario (
  id_usuario NUMBER(10) NOT NULL,
  nome VARCHAR2(150) NOT NULL,
  cpf VARCHAR2(30) NOT NULL,
@@ -16,15 +16,14 @@ CREATE TABLE IF NOT EXISTS n_usuarios (
  CONSTRAINT pk_usuario PRIMARY KEY (id_usuario)
 );
 commit;
-CREATE TABLE IF NOT EXISTS n_itens (
+CREATE TABLE IF NOT EXISTS n_item (
  id_item NUMBER(10) NOT NULL,
  item_nome VARCHAR2(150) NOT NULL,
- quantidade NUMBER(10) NOT NULL,
  id_categoria NUMBER(10) NOT NULL,
  id_un_medida NUMBER(10) NOT NULL,
  CONSTRAINT pk_item PRIMARY KEY (id_item),
- CONSTRAINT fk_categoria FOREIGN KEY (id_categoria) REFERENCES n_categorias(id_categoria),
- CONSTRAINT fk_un_medida FOREIGN KEY (id_un_medida) REFERENCES n_unidade_medida(id_unidade_medida)
+ CONSTRAINT fk_categoria FOREIGN KEY (id_categoria) REFERENCES n_categoria (id_categoria),
+ CONSTRAINT fk_un_medida FOREIGN KEY (id_un_medida) REFERENCES n_unidade_medida (id_unidade_medida)
 );
 
 CREATE TABLE IF NOT EXISTS n_unidade_medida (
@@ -33,16 +32,17 @@ CREATE TABLE IF NOT EXISTS n_unidade_medida (
  CONSTRAINT pk_un_medida PRIMARY KEY (id_unidade_medida)
 );
 
-CREATE TABLE IF NOT EXISTS n_categorias (
+CREATE TABLE IF NOT EXISTS n_categoria (
  id_categoria NUMBER(10) NOT NULL,
  categoria_nome VARCHAR2(100),
  CONSTRAINT pk_categoria PRIMARY KEY (id_categoria)
 );
 
-CREATE TABLE IF NOT EXISTS n_lotes (
+CREATE TABLE IF NOT EXISTS n_lote (
  id_lote NUMBER(10) NOT NULL,
  id_item NUMBER(10) NOT NULL,
+ quantidade NUMBER(10) NOT NULL,
  data_validade DATE NOT NULL,
  CONSTRAINT pk_lote PRIMARY KEY (id_lote),
- CONSTRAINT fk_item FOREIGN KEY (id_item) REFERENCES n_itens(id_item)
+ CONSTRAINT fk_item FOREIGN KEY (id_item) REFERENCES n_item (id_item)
 );
