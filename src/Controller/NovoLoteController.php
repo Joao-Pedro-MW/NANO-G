7ddc9f3d055eb8da1ded47dc8573a91cff3bf2d0
João Pedro MW <jpedro01230@gmail.com>
2023-10-31 19:08:07 -0300

    Revert "REVERTER MUDANÇAS DEPOIS!!!"
    This reverts commit f12c66e355b42a01d41a80afbd78ffa89f5477cc.

--- src/Controller/NovoLoteController.php
+++ src/Controller/NovoLoteController.php
@@ -2,25 +2,22 @@
 
 namespace src\Controller;
 use src\Repository\Itens\ItensRepository;
-use src\Repository\Titan;
 use src\Repository\UnidadesMedida\UnidadesMedidaRepository;
 use src\Controller\Controller;
 
 class NovoLoteController implements Controller
 {
-    public function __construct(private Titan $Titan)
+    public function __construct(private ItensRepository $itensRepository)
     {
-        $this->Titan = $Titan;
     }
     public function processaRequisicao(): void
     {
-        $listaUnidadesMedida = $this->Titan->RetornaLoteRepository();
-        var_dump($listaUnidadesMedida);
+        $unidadesMedida =
         //Pegamos os produtos necessários
-        //$listaProdutos = $this->itensRepository->TodosProdutos();
+        $listaProdutos = $this->itensRepository->TodosProdutos();
 
         //Pegamos as unidades de medida que vamos usar
-        //$listaUnidadesMedida =
-        //require_once __DIR__ . '\..\..\src\Views\Item\cadastro_lote.php';
+        $listaUnidadesMedida =
+        require_once __DIR__ . '\..\..\src\Views\Item\cadastro_lote.php';
     }
 }
--- src/Repository/UnidadesMedida/UnidadesMedidaRepository.php
+++ src/Repository/UnidadesMedida/UnidadesMedidaRepository.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace src\Repository\UnidadesMedida;
+namespace src\Repository;
 use PDO;
 class UnidadesMedidaRepository
 {
