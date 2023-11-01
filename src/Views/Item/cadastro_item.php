49bc22f3d82ba9ae3a87b777d71609b0d4e52421
Heblg <victorheblg@gmail.com>
2023-10-31 22:50:04 -0300

    TA PRONTA A PORRA DA TELA JOAO VAI TRABALHAR AGORA CARALHO

--- src/Views/Item/cadastro_item.php
+++ src/Views/Item/cadastro_item.php
@@ -10,8 +10,29 @@
 </head>
 
 <body>
-<?php require __DIR__ . '..\..\..\menu_lateral.php' ?>
-<h1>Cadastro de novo item</h1>
+	<?php require __DIR__ . '..\..\..\menu_lateral.php' ?>
 
+    <div class="main_div">
+        <form action="" method="GET" id="form_item">
+
+            <div class="content_div">
+                    <label for="nome">Nome</label>
+                    <input id="nome" type="text" class="input">
+
+                    <label for="categoria">Categoria</label>
+                    <input id="categoria" type="text" class="input">
+
+                    <label for="unidade_medida">Unidade de medida</label>
+                    <select id="unidade_medida" class="input_combo">
+                        <option value="">1</option>
+                        <option value="">2</option>
+                        <option value="">3</option>
+                    </select>
+
+                    <input type="submit" value="Cadastrar item" class="botao">
+            </div>
+
+        </form>
+    </div>
 </body>
 </html>
