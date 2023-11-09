0164ade40604ad3984811934661d5ea9a84b37f8
João Pedro MW <jpedro01230@gmail.com>
2023-11-08 23:28:23 -0300

    Ajustado Menu Itens

--- src/Views/Item/menu_itens.php
+++ src/Views/Item/menu_itens.php
@@ -16,8 +16,8 @@
     <div class="head">
 
         <h1>Consulta de itens</h1>
-        
-        <a href="/itens/cadastro_item"> 
+
+        <a href="/itens/cadastro_item">
             <button type="button" class="botao">Cadastrar novo item</button>
         </a>
 
@@ -31,41 +31,26 @@
             <th>Operações</th>
         </tr>
 
-<<<<<<< HEAD
-                <td class="td_produto"><?= $item["ITEM_NOME"] ?></td>
-                <td class="td_un_medida"><?= $item["ID_CATEGORIA"] ?></td>
-                <td class="td_tipo"><?= $item["ID_UN_MEDIDA"] ?></td>
-                <td class="td_opcoes">
-                    <a style="float:right;" class="botao_editar"
-                       href="/itens/edita_item?item=<?= $item["ID_ITEM"] ?>"><img
-                                src="..\imagens\imagem_lapis_edicao.svg" title="Editar item" alt="Lapis" id="icones">
-                        <a style="float:right;" class="botao_lixeira"
-                           href="/itens/remove_item?item=<?= $item["ID_ITEM"] ?>"> <img
-                                    src="..\imagens\imagem_lixeira.svg" title="Excluir item" alt="Lixeira" id="icones">
-                </td>
-            </tr>
-=======
         <?php foreach ($listaItens as $item): ?>
 
-        <tr>
+            <tr>
 
-            <td class="td_produto" id="nome"><?= $item["ITEM_NOME"] ?></td>
-            <td class="td_un_medida" id="un_medida"v><?= $item["ID_CATEGORIA"] ?></td>
-            <td class="td_tipo" id="tipo"><?= $item["ID_UN_MEDIDA"] ?></td>
+                <td class="td_produto" id="nome"><?= $item["ITEM_NOME"] ?></td>
+                <td class="td_un_medida" id="un_medida"v><?= $item["ID_CATEGORIA"] ?></td>
+                <td class="td_tipo" id="tipo"><?= $item["ID_UN_MEDIDA"] ?></td>
 
-            <td class="td_opcoes">
-                
-                <a class="botao_editar" href="/itens/edita_item?item=<?= $item["ID_ITEM"] ?>">
-                    <img src="..\imagens\imagem_lapis_edicao.svg" title="Editar item" alt="Lapis" id="icones">
-                </a>
+                <td class="td_opcoes">
 
-                <a id="lixeira" class="botao_lixeira" href="/itens/remove_item?item=<?= $item["ID_ITEM"] ?>"> 
-                    <img src="..\imagens\imagem_lixeira.svg" title="Excluir item" alt="Lixeira" id="icones">
-                </a>
+                    <a class="botao_editar" href="/itens/edita_item?item=<?= $item["ID_ITEM"] ?>">
+                        <img src="..\imagens\imagem_lapis_edicao.svg" title="Editar item" alt="Lapis" id="icones">
+                    </a>
 
-            </td>
-        </tr>
->>>>>>> f8c8eeb9795e7f79e095ed6967ff376d7698ec1f
+                    <a id="lixeira" class="botao_lixeira" href="/itens/remove_item?item=<?= $item["ID_ITEM"] ?>">
+                        <img src="..\imagens\imagem_lixeira.svg" title="Excluir item" alt="Lixeira" id="icones">
+                    </a>
+
+                </td>
+            </tr>
         <?php endforeach ?>
     </table>
 </div>
