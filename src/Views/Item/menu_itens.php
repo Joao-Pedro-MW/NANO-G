fb385a5d8c704f4f1d825b8578d52fde371b4058
Alynne Ayumi Kuriyama <alynneak@outlook.com>
2023-11-08 23:07:35 -0300

    voltar_query

--- src/Views/Item/menu_itens.php
+++ src/Views/Item/menu_itens.php
@@ -24,9 +24,9 @@
         <?php foreach ($listaItens as $item): ?>
             <tr>
 
-                <td class="td_produto"><?= $item["it.ITEM_NOME"] ?></td>
-                <td class="td_un_medida"><?= $item["c.CATEGORIA_NOME"] ?></td>
-                <td class="td_tipo"><?= $item["um.UNIDADE_NOME"] ?></td>
+                <td class="td_produto"><?= $item["ITEM_NOME"] ?></td>
+                <td class="td_un_medida"><?= $item["ID_CATEGORIA"] ?></td>
+                <td class="td_tipo"><?= $item["ID_UN_MEDIDA"] ?></td>
                 <td class="td_opcoes">
                     <a style="float:right;" class="botao_editar"
                        href="/itens/edita_item?item=<?= $item["ID_ITEM"] ?>"><img
